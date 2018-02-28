ee7bb6162fe85c1d13f6fcd383bd851cb2b807c4
Ferdinand Prantl <prantlf@gmail.com>
2018-02-28 10:08:30 +0100

    chore: Fix merging error

--- tasks/instructions/hasClass.js
+++ tasks/instructions/hasClass.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 module.exports = {
@@ -28,34 +27,3 @@ module.exports = {
       });
   }
 };
-||||||| merged common ancestors
-=======
-'use strict';
-
-module.exports = {
-  detect: function (command) {
-    return !!command.hasClass;
-  },
-
-  perform: function (grunt, target, client, command) {
-    const hasClass = command.hasClass,
-          selector = hasClass.selector,
-          expected = hasClass.value || '',
-          expectedList = expected.split(/ +/);
-    grunt.log.ok('Looking for classes "' + expected + '" at "' +
-                 selector + '".');
-    return client.getAttribute(selector, 'class')
-      .then(function (actual) {
-        const actualList = (actual || '').split(/ +/);
-        if (!expectedList.some(function (expected) {
-              return expected.startsWith('!') ?
-                actualList.indexOf(expected.substr(1)) < 0 :
-                actualList.indexOf(expected) >= 0;
-            })) {
-          throw new Error ('Classes "' + expected +
-            '" were not found among "' + actual + '".at "' + selector + '".');
-        }
-      });
-  }
-};
->>>>>>> ed18031946de405cd074c973e2461622016d7c31
