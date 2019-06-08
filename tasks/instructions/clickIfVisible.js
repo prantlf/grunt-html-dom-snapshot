34a3a72fbb21effcab8ce1cb796b4bfbd81b0964
Ferdinand Prantl <prantlf@gmail.com>
2019-06-08 02:35:48 +0200

    chore: Fix coding standard and logging

--- tasks/instructions/clickIfVisible.js
+++ tasks/instructions/clickIfVisible.js
@@ -2,20 +2,20 @@
 
 module.exports = {
   detect: function (command) {
-    return !!command.clickIfVisible;
+    return !!command.clickIfVisible
   },
 
   perform: function (grunt, target, client, command) {
-    console.log('target "' + target + '".');
-    const clickIfVisible = command.clickIfVisible;
-   client.isVisible(clickIfVisible).then(function(value) {
-     if (value === true) {
-        grunt.verbose.writeln('ClickIfVisible on "' + clickIfVisible + '".');
-        return client.click(clickIfVisible);
-      }else{
-        grunt.verbose.writeln('ClickIfVisible on "' + clickIfVisible + '" skipped because control is not visible.');
-
+    console.log('target "' + target + '".')
+    const clickIfVisible = command.clickIfVisible
+    grunt.verbose.writeln('Click on "' + clickIfVisible + '" if visible.')
+    client.isVisible(clickIfVisible).then(function (value) {
+      if (value === true) {
+        grunt.verbose.writeln('Visible: "' + clickIfVisible + '".')
+        return client.click(clickIfVisible)
+      } else {
+        grunt.verbose.writeln('Not visible: "' + clickIfVisible + '".')
       }
-    });
+    })
   }
 }
